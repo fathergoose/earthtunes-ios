ea1c35f69d2f5512bf393d55b3e25897e6472c82
Al Ilseman <al@ilseman.com>
2019-11-23 13:14:13 -0600

    Initial Commit

---
+++ EarthTunes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EarthTunes
+//
+//  Created by Al Ilseman on 11/23/19.
+//  Copyright © 2019 Listening-To-The-Solid-Earth. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
